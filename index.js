8c6070f10e07b7f10dad868decb117db4a1dff08
Virginia <vmaciasromero@gmail.com>
2019-11-24 21:31:24 +0100

    Added support to nedb for POST method

--- index.js
+++ index.js
@@ -3,19 +3,26 @@
 
 var express= require("express");
 var bodyParser = require("body-parser");
+var DataStore=require ("nedb");
 
 var port=3000;
 var BASE_API_PATH ="/api/v1";
+var DB_FILE_NAME= __dirname + "/contacts.json";
+// var contacts=[
+//     {"name": "peter", "phone":"1234"},
+//     {"name": "john", "phone": "6666"}
+// ];
 
-var contacts=[
-    {"name": "peter", "phone":"1234"},
-    {"name": "john", "phone": "6666"}
-];
-
-console.log("starting API server..");
+console.log("Starting API server..");
 
 var app = express();
 app.use(bodyParser.json());
+
+//Inicializar la BD nebd
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true
+});
 
 app.get("/", (req,res)=> {
     res.send("<html><body><h1>My server</h1></body></html");
@@ -23,15 +30,22 @@ app.get("/", (req,res)=> {
 
 app.get(BASE_API_PATH + "/contacts", (req, res)=>{
     console.log(Date() + "- GET /contacts");
-    res.send(contacts);
+    res.send([]);  //contacts
 });
 
 app.post(BASE_API_PATH + "/contacts", (req, res)=>{
     console.log(Date() + "- POST /contacts");
     var contact =req.body;
-    contacts.push(contact);
-    res.sendStatus(201);
-})
+    // contacts.push(contact);
+    db.insert(contact, (err)=>{
+        if (err){
+            console.log(Date() + " - " + err);
+            res.sendStatus(500);
+        }else{
+            res.sendStatus(201);
+        }
+    });
+});
 
 app.listen(port);
-console.log("server ready");
+console.log("server ready!");
